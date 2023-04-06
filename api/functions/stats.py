bdc3a6bfac461598742c4f5ab8daf8f7911601c3
Hugo Borsoni <hugo.borsoni@student-cs.fr>
2023-04-06 11:09:36 +0200

    parametrized and added 8 quadrants function

--- api/functions/stats.py
+++ api/functions/stats.py
@@ -3,23 +3,59 @@ def getStats(point,surroundingPoints):
     min 
     return {'mean':sum(alts)/len(alts),'max':max(alts),'min':min(alts)}
 
-def tripleCircleBassinVersant(point,pointAlti,circle1,circle2,circle3):
+
+def tripleCircleBassinVersant(point,pointAlti,circles):
     surfaceCount=0
-    NW = [[pt for pt in circle if pt['lat']>=point.lat and pt['lon']<point.long] for circle in [circle1,circle2,circle3]]
-    NE = [[pt for pt in circle if pt['lat']>=point.lat and pt['lon']>=point.long] for circle in [circle1,circle2,circle3]]
-    SE = [[pt for pt in circle if pt['lat']<point.lat and pt['lon']<point.long] for circle in [circle1,circle2,circle3]]
-    SW = [[pt for pt in circle if pt['lat']<point.lat and pt['lon']>=point.long] for circle in [circle1,circle2,circle3]]
     
-    for quadrant in [NW,NE,SE,SW]:
-        meanAlti1 = sum([pt['z'] for pt in quadrant[0]])/len(quadrant[0])
-        if meanAlti1>1.02*pointAlti:
-            surfaceCount+=1
-            meanAlti3 = sum([pt['z'] for pt in quadrant[1]])/len(quadrant[1])
-            if meanAlti3>1.05*meanAlti1:
-                surfaceCount+=8
-                meanAlti5 = sum([pt['z'] for pt in quadrant[2]])/len(quadrant[2])
-                if meanAlti5>1.05*meanAlti3:
-                    surfaceCount+=16
+    for quadrant in quadrantsGenerator(point,circles,eightQuadrants=False):
+        surfaceCount += nextCircleCheck(pointAlti,quadrant)
     
-    return surfaceCount/100
+    return surfaceCount/100 
+
+
+def nextCircleCheck(alti,quadrant,index=0,score=0,addedScore={0:1,1:8,2:16},elevationPercentage={0:1.02,1:1.05,2:1.05}):
+        
+    if index==len(quadrant):
+        return score
     
+    meanAlti = sum([pt['z'] for pt in quadrant[index]])/len(quadrant[index])
+    if meanAlti>elevationPercentage[index]*alti:
+        score+=addedScore[index]
+        return nextCircleCheck(meanAlti,quadrant,index=index+1,score=score)
+    return score
+    
+
+def quadrantsGenerator(centerPoint,circles,eightQuadrants=False):
+    quadrants=[]
+    if not eightQuadrants:
+        for q in ['NW','NE','SE','SW']:
+            quadrants.append([[pt for pt in circle if isQuadrant(q,centerPoint,pt)] for circle in circles])
+    else:
+        for i,q in enumerate(['NW','NE','SE','SW']):
+            quadrants.append([[pt for pt in circle if isQuadrant(q,centerPoint,pt) and hasMovedMoreLat(centerPoint,pt,includeEqual=i%2)] for circle in circles])
+            quadrants.append([[pt for pt in circle if isQuadrant(q,centerPoint,pt) and hasMovedMoreLong(centerPoint,pt,includeEqual=(i-1)%2)] for circle in circles])
+        # resultat : NNW,NWW,NNE,NEE,SSE,SEE,SSW,SWW, avec une chacun frontière avec condition de type '>=' et une de type '>'
+    return quadrants
+
+
+def isQuadrant(quadrant,centerPoint,pt):
+    match quadrant:
+        case 'NW':
+            return pt['lat']>=centerPoint.lat and pt['lon']<centerPoint.long
+        case 'NE':
+            return pt['lat']>centerPoint.lat and pt['lon']>=centerPoint.long
+        case 'SE':
+            return pt['lat']<=centerPoint.lat and pt['lon']>centerPoint.long
+        case 'SW':
+            return pt['lat']<centerPoint.lat and pt['lon']>=centerPoint.long
+
+
+def hasMovedMoreLong(centerPoint,pt,includeEqual=False):
+    if includeEqual:
+        return abs(centerPoint.long-pt['long'])>=abs(centerPoint.lat-pt['lat'])
+    return abs(centerPoint.long-pt['long'])>abs(centerPoint.lat-pt['lat'])
+
+def hasMovedMoreLat(centerPoint,pt,includeEqual=False): # on crée la fonction antagoniste par lisibilité du code
+    if includeEqual:
+        return abs(centerPoint.lat-pt['lat'])>=abs(centerPoint.long-pt['long'])
+    return abs(centerPoint.lat-pt['lat'])>abs(centerPoint.long-pt['long'])
--- api/main.py
+++ api/main.py
@@ -60,7 +60,7 @@ async def store_search(lat: float, long: float, api:str="IGN"):
     return res
 
 @app.get("/surroundings", status_code=200)
-async def surroundings(lat: str, long: str):
+async def surroundings(lat: str, long: str):# -> SurroundingsResponse:
     point = Point(lat,long)
     pts = point.createDonutGridAround(100,300)
     print(len(pts))
@@ -70,7 +70,7 @@ async def surroundings(lat: str, long: str):
     return SurroundingsResponse(success=True,points=res,dataset=altiQueryService.dataSet)
 
 @app.get("/surroundings/square", status_code=200)
-async def surroundings_square(lat: str, long: str,sidePointsNb:int=10,separationDistance:int=10):
+async def surroundings_square(lat: str, long: str,sidePointsNb:int=10,separationDistance:int=10): #-> SurroundingsSquareResponse:
     point = Point(lat,long)
     pts = point.createSquareGridAround(sidePointsNb,separationDistance)
     altiQueryService = altiQuery("IGN")
@@ -84,7 +84,7 @@ async def surroundings_square(lat: str, long: str,sidePointsNb:int=10,separation
         stats=stats)
 
 @app.get("/surroundings/circle", status_code=200)
-async def surroundings_circle(lat: str, long: str,radius:int=100):
+async def surroundings_circle(lat: str, long: str,radius:int=100):# -> SurroundingsCircleResponse:
     point = Point(lat,long)
     pts = point.createRoundGridAround(radius)
     altiQueryService = altiQuery("IGN")
@@ -98,7 +98,7 @@ async def surroundings_circle(lat: str, long: str,radius:int=100):
         stats=stats)
 
 @app.get("/surroundings/triple-circle", status_code=200)
-async def surroundings_triple_circle(lat: str, long: str,radius:int=100):
+async def surroundings_triple_circle(lat: str, long: str,radius:int=100):# -> SurroundingsTripleCircleResponse:
     point = Point(lat,long)
     pts1 = point.createRoundGridAround(radius)
     pts3 = point.createDonutGridAround(radius,3*radius)
@@ -109,7 +109,7 @@ async def surroundings_triple_circle(lat: str, long: str,radius:int=100):
     res5 = altiQueryService.QueryMultiplePoints(pts5)
     stats = getStats(point,res1+res3+res5)
     altiResponse = altiQueryService.QueryOnePoint(point.gps.lat,point.gps.long)
-    indicator = tripleCircleBassinVersant(point.gps,altiResponse.alti,res1,res3,res5)
+    indicator = tripleCircleBassinVersant(point.gps,altiResponse.alti,[res1,res3,res5])
     return SurroundingsTripleCircleResponse(
         success=True,
         center=point.gps.toJSON(),
